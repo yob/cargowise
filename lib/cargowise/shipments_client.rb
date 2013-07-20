321b1abc9dcf3ba4332db9eb147e0d654be264a5
James Healy <james@yob.id.au>
2013-07-20 17:52:34 +1000

    comment settings used to build a savon client

--- lib/cargowise/shipments_client.rb
+++ lib/cargowise/shipments_client.rb
@@ -36,10 +36,23 @@ module Cargowise
         wsdl: wsdl_path,
         endpoint: endpoint_uri,
 
+        # Cargowise servers can be super slow to respond, this gives them time
+        # to have a smoko before responding to our queries.
         read_timeout: 120,
+
+        # OHL uses cargowise and has a load balancer that freaks out if we use
+        # the OpenSSL 1.0.1 default of TLS1.1.
         ssl_version: :TLSv1,
+
+        # savon 2.2.0 ignores the above ssl_version unless this is set to
+        # false. Annoying.
         ssl_verify_mode: :none,
 
+        # turn off logging to keep me sane. Change this to true when developing
+        log: false,
+
+        # the cargowsie API requires auth details in the SOAP header of every
+        # request
         soap_header: {
           "tns:WebTrackerSOAPHeader" => {
             "tns:CompanyCode" => company_code,
